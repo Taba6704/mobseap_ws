12c58d5f3b5d4fb158807acb55e39778f7458e58
MOBSEA <BASSOT@my.erau.edu>
2026-04-27 17:38:43 -0400

    added loop that waits until OAK cam bootup until launching

--- start_panda.sh
+++ start_panda.sh
@@ -5,5 +5,19 @@ source /home/mobseap/mobseap_ws/install/setup.bash
 export ROS_DOMAIN_ID=123
 export ROS_LOCALHOST_ONLY=0
 export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
+export LD_LIBRARY_PATh=/usr/local/lib:/opt/ros/humble/lib:$LD_LIBRARY_PATH
 
+echo "Waiting for OAK Camera..."
+
+for i in {1..60}; do
+    if oakctl list 2>/dev/null | grep -q "OAK4-D"; then
+        echo "OAK Camera Detected."
+        break
+    fi
+
+    echo "OAK not detected yet... attempt $i/60"
+    sleep 2
+done
+
+echo "Starting LattePanda bringup..."
 exec ros2 launch lattepanda_bringup panda_bringup.launch.py
